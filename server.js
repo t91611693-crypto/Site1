f762f84c1da7e20fbd03f192078e3741c50b9add
Test <91611693@gmail.com>
2026-03-02 19:35:55 +0530

    Fix deployment routing

--- server.js
+++ server.js
@@ -32,3 +32,21 @@ app.post("/api/subscribe", (req, res) => {
 
   res.json({ message: "Subscribed successfully!" });
 });
+const express = require("express");
+const path = require("path");
+
+const app = express();
+
+// Serve static files
+app.use(express.static(__dirname));
+
+// Route for homepage
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on ${PORT}`);
+});
